56d575ce2ed5f7977f6c5393b89769dbed50c1b5
fakefeik <feikfake1@gmail.com>
2017-03-12 18:33:08 +0500

    Use connection string from ENV

--- model/init.js
+++ model/init.js
@@ -2,11 +2,23 @@
 var Sequelize = require('sequelize');
 var pg = require('pg');
 
+function getSequelize(connectionString) {
+    return new Sequelize(connectionString, {
+        dialectOptions: { charset: 'utf8' }
+    });
+}
+
 module.exports.init = function (callback) {
     var dbName = 'quest';
     var username = 'postgres';
     var password = 'qwer';
     var host = 'localhost';
+
+    var connectionString = process.env.CONNECTION_STRING;
+    if (connectionString) {
+        callback(getSequelize(connectionString));
+        return;
+    }
 
     var connectToPostgres = `postgres://${username}:${password}@${host}/postgres`;
     var connectToDb = `postgres://${username}:${password}@${host}/${dbName}`;
