d9b14338e61bf7431e0a0855db973e27a66d0a55
fakefeik <feikfake1@gmail.com>
2017-03-25 19:24:49 +0500

    Remove jade view engine

--- app.js
+++ app.js
@@ -5,29 +5,21 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
-const quests = require('./routes/quests');
+const api = require('./routes/api');
 
 const app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-let publicDir = process.env.NODE_ENV === 'local'
-    ? path.join(__dirname, 'public')
-    : "//team1.surge.sh";
+let publicDir = process.env.NODE_ENV === 'production'
+    ?'//team1.surge.sh'
+    : path.join(__dirname, 'public');
 
-app.use(express.static(publicDir));
-
-app.use('/', index);
-app.use('/users', users);
-app.use('/quests', quests);
+app.use('/', express.static(publicDir));
+app.use('/api', api);
 
 app.use(function (req, res, next) {
     const err = new Error('Not Found');
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const database = require('../model/database');
 
 const router = express.Router(); // eslint-disable-line
 
-router.get('/', function (req, res) {
+router.get('/quests', function (req, res) {
     database.Quest.findAll()
         .then(function (quests) {
             res.send(JSON.stringify(quests));
