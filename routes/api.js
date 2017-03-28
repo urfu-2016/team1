f45b8bc8b7b21dadba20669c36d3ae44ebfcface
eoL95 <bragin.egor.95@gmail.com>
2017-03-28 20:44:22 +0500

    Find by first letters

--- routes/api.js
+++ routes/api.js
@@ -2,40 +2,25 @@
 const express = require('express');
 const database = require('../model/database');
 
-const router = express.Router(); // eslint-disable-line
+const router = express.Router();
 
 router.get('/quests', function (req, res) {
     database.Quest.findAll()
-        .then(function (quests) {
-            res.send(JSON.stringify(quests));
-        });
+        .then(quests => res.json(quests));
 });
 
 router.get('/quests/id/:id', function (req, res) {
     const id = req.params.id;
     database.Quest.findById(id)
-        .then(function(quest) {
-            res.send(JSON.stringify(quest));
-        });
+        .then(quest => res.json(quest));
 });
 
 router.get('/quests/name/:name', function (req, res) {
     const name = req.params.name;
-    database.Quest.findOne({ where: {title: name} })
-        .then(function (quest) {
-            res.send(JSON.stringify(quest));
-        });
-});
-
-router.get('/quests/:name', function (req, res) {
-    const name = req.params.name;
     database.Quest.findAll()
-        .then(function (quests) {
-            res.send(JSON.stringify(quests.filter(function (quest) {
-                return quest.title.startsWith(name);
-            })));
+        .then(quests => {
+            res.json(quests.filter(quest => quest.title.startsWith(name)));
         });
-
 });
 
 module.exports = router;
