913fc196a3f6ce2c692077736f928a7b1d2376bd
fakefeik <feikfake1@gmail.com>
2017-03-30 00:20:42 +0500

    Repair app start

--- webpack.devconf.js
+++ webpack.devconf.js
@@ -47,7 +47,7 @@ module.exports = {
             },
             {
                 test: /\.js$/,
-                exclude: /\/node_modules\//,
+                exclude: /node_modules/,
                 loader: 'babel',
                 query: {
                     presets: ['es2015', 'react'],
--- webpack.prodconf.js
+++ webpack.prodconf.js
@@ -38,7 +38,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js$/,
-                exclude: /\/node_modules\//,
+                exclude: /node_modules/,
                 loader: 'babel',
                 query: {
                     presets: ['es2015', 'react'],
