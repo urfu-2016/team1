c75d4d27cb8976a5c0ba054d0877ed982877e60b
Gulchuk Petr <P.Gulchuk@yandex.ru>
2017-03-30 01:04:18 +0500

    change questItem block

--- views/component/pages/questlist/questItem/QuestItem.js
+++ views/component/pages/questlist/questItem/QuestItem.js
@@ -9,23 +9,21 @@ export default class QuestItem extends React.Component {
             <div className='quest-item'>
                 <div className='quest-item__row'>
                     <Link to='/question' className='quest-item__item'>
-                            <div>
-                                <img src={plug} alt='plug'/>
-                                <div className='quest-item__item-info'>
-                                    <h2>Question Title</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius est nam rerum? Adipisci culpa ipsum placeat voluptatum. Alias architecto consectetur consequatur dolore eveniet illum, libero molestias nostrum, obcaecati quam recusandae, suscipit voluptatum. Beatae culpa esse ipsam maxime provident quae rem?</p>
-                                </div>
+                        <div>
+                            <img src={plug} alt='plug'/>
+                            <div className='quest-item__item-info'>
+                                <h2>Question Title</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius est nam rerum? Adipisci culpa ipsum placeat voluptatum. Alias architecto consectetur consequatur dolore eveniet illum, libero molestias nostrum, obcaecati quam recusandae, suscipit voluptatum. Beatae culpa esse ipsam maxime provident quae rem?</p>
                             </div>
+                        </div>
                     </Link>
 
                     <Link to='/question' className='quest-item__item'>
-                        <div >
-                            <div>
-                                <img src={plug} alt='plug'/>
-                                <div className='quest-item__item-info'>
-                                    <h2>Question Title</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius est nam rerum? Adipisci culpa ipsum placeat voluptatum. Alias architecto consectetur consequatur dolore eveniet illum, libero molestias nostrum, obcaecati quam recusandae, suscipit voluptatum. Beatae culpa esse ipsam maxime provident quae rem?</p>
-                                </div>
+                        <div>
+                            <img src={plug} alt='plug'/>
+                            <div className='quest-item__item-info'>
+                                <h2>Question Title</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius est nam rerum? Adipisci culpa ipsum placeat voluptatum. Alias architecto consectetur consequatur dolore eveniet illum, libero molestias nostrum, obcaecati quam recusandae, suscipit voluptatum. Beatae culpa esse ipsam maxime provident quae rem?</p>
                             </div>
                         </div>
                     </Link>
