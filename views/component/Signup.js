9a69d833dac6d3cf052e738462a10f4f53dedd4d
fakefeik <feikfake1@gmail.com>
2017-04-04 20:36:54 +0500

    Add form to Signup

--- views/component/Signup.js
+++ views/component/Signup.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
-export default class Repos extends React.Component {
+export default class Signup extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.loginInput = ref => {this._loginInput = ref;};
+        this.passInput = ref => {this._passInput = ref;};
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(`Отправлено значение: ${this._loginInput.value}, ${this._passInput.value}`);
+    }
+
     render() {
         return (
             <div>
                 <h2>Signup</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input ref={this.loginInput} placeholder='login'/>
+                    <input ref={this.passInput} placeholder='password'/>
+                    <input type='submit'/>
+                </form>
             </div>
-        );
+        ) ;
     }
 }
-
