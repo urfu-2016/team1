140a2bdc73f336d8593af4fbc979658b966f86e4
slaeme <slaeme@yandex.ru>
2017-04-16 17:18:25 +0500

    refactor test data

--- tools/database/test-data.js
+++ tools/database/test-data.js
@@ -2,48 +2,55 @@
 
 const data = {};
 
-data.users = [{
-    login: 'JIM',
-    password: 'a_secret'
-},
-{
-    login: 'BIM',
-    password: 'a_secret'
-}];
+data.users = [
+    {
+        login: 'JIM',
+        password: 'a_secret'
+    },
+    {
+        login: 'BIM',
+        password: 'a_secret'
+    }
+];
 
-data.quests = [{
-    title: 'Elmash',
-    description: 'description quest1'
-},
-{
-    title: 'VIZ',
-    description: 'description quest2'
-},
-{
-    title: 'CENTER',
-    description: 'description quest3'
-}];
+data.quests = [
+    {
+        title: 'Elmash',
+        description: 'description quest1'
+    },
+    {
+        title: 'VIZ',
+        description: 'description quest2'
+    },
+    {
+        title: 'CENTER',
+        description: 'description quest3'
+    }
+];
 
-data.places = [{
-    name: 'OFFICE YANDEX',
-    description: 'Some description',
-    coordinates: 'while string',
-    photo:  'url for load photo'
-},
-{
-    name: 'METRO URALMASH',
-    description: 'Some description',
-    coordinates: 'while string',
-    photo:  'url for load photo'
-},
-{
-    name: 'VIZ BULVAR',
-    description: 'Some description',
-    coordinates: 'while string',
-    photo:  'url for load photo'
-}];
+data.places = [
+    {
+        name: 'OFFICE YANDEX',
+        description: 'Some description',
+        coordinates: 'while string',
+        photo:  'url for load photo'
+    },
+    {
+        name: 'METRO URALMASH',
+        description: 'Some description',
+        coordinates: 'while string',
+        photo:  'url for load photo'
+    },
+    {
+        name: 'VIZ BULVAR',
+        description: 'Some description',
+        coordinates: 'while string',
+        photo:  'url for load photo'
+    }
+];
 
-data.photos = [{
+data.photos = [
+    {
         url: "some url for photo 1"
     },
     {
@@ -51,17 +58,20 @@ data.photos = [{
     },
     {
         url: "some url for photo 3"
-    }];
+    }
+];
 
-data.comments = [{
-    text: "some comment 1"
-},
-{
-    text: "some comment 2"
-},
-{
-    text: "some comment 3"
-}];
+data.comments = [
+    {
+        text: "some comment 1"
+    },
+    {
+        text: "some comment 2"
+    },
+    {
+        text: "some comment 3"
+    }
+];
 
 data.likes = [{}, {}, {}, {}, {}];
 
