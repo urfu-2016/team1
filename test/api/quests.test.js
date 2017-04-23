38ac76cf6989f06cd1425b22d08dacba31d2c991
fakefeik <feikfake1@gmail.com>
2017-04-23 21:14:42 +0500

    Add api tests for quest search by name & id

--- test/api/quests.test.js
+++ test/api/quests.test.js
@@ -13,15 +13,15 @@ const { before, describe, it } = bdd;
 class ApiQuestsTest extends ApiTestBase {
     @before
     setUpDatabase(done) {
-        let quests = [
+        this.quests = [
             {title: 'FirstQuest', description: 'no description'},
             {title: 'SecondQuest', description: 'second description'},
             {title: 'ThirdQuest', description: 'really long long long long long long long long long long long long long long long long long long long long long long long long long description'},
             {title: 'FourthQuest', description: 'no description'},
             {title: 'FifthQuest', description: 'no description'},
-            {title: 'SixthQuest', description: 'no description'},
+            {title: 'sixthQuest', description: 'no description'}
         ];
-        models.Quest.truncate().then(() => models.Quest.bulkCreate(quests).then(() => done()));
+        models.Quest.truncate().then(() => models.Quest.bulkCreate(this.quests).then(() => done()));
     }
 
     @it('should return all quests')
@@ -32,7 +32,44 @@ class ApiQuestsTest extends ApiTestBase {
                 res.should.have.status(200);
                 let quests = res.body;
                 quests.should.have.lengthOf(6);
+                quests.map(x => ({title: x.title, description: x.description})).should.deep.equal(this.quests);
                 done();
+            });
+    }
+
+    @it('should search by name')
+    searchByName(done) {
+        chai.request(this.server)
+            .get('/api/quests/name/s')
+            .end((err, res) => {
+                res.should.have.status(200);
+                let quests = res.body;
+                console.log(quests);
+                quests.should.have.lengthOf(2);
+                quests.map(x => ({title: x.title, description: x.description})).should.deep.equal([
+                    {title: 'SecondQuest', description: 'second description'},
+                    {title: 'sixthQuest', description: 'no description'}
+                ]);
+                done();
+            });
+    }
+
+    @it('should search by id')
+    searchById(done) {
+        chai.request(this.server)
+            .get('/api/quests/name/SecondQuest')
+            .end((err, res) => {
+                res.should.have.status(200);
+                let quests = res.body;
+                quests.should.have.lengthOf(1);
+                let quest = quests[0];
+                chai.request(this.server)
+                    .get(`/api/quests/id/${quest.id}`)
+                    .end((err, res) => {
+                        let questById = res.body;
+                        questById.should.be.deep.equal(quest);
+                        done();
+                    });
             });
     }
 }
