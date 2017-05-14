b8119a9df2334c4ff0ba25e4bcaaad33fc03db7a
fakefeik <feikfake1@gmail.com>
2017-05-14 13:24:56 +0500

    Add drop tables script for prod

--- tools/database/create.js
+++ tools/database/create.js
@@ -1,14 +1,3 @@
-'use strict';
-const pg = require('pg');
-const path = require('path');
-const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '..', '..', 'config', 'config.json'))[env];
-const dbConfig = config.database;
-const uriToPostgres = dbConfig.options.dialect + '://' + dbConfig.username + ':' + dbConfig.password
-    + '@' + dbConfig.options.host + '/' + dbConfig.options.dialect;
+const models = require('../../models');
 
-pg.connect(uriToPostgres, function (err, client, done) {
-    client.query('CREATE DATABASE ' + config.database.dbName, function (err) {
-        client.end();
-    });
-});
+models.sequelize.sync({force: true});
